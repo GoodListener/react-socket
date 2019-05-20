1bc5374b187f4f83ff994f4f8f929f5631e8a1ca
ekim <good_listener@naver.com>
2019-05-20 23:41:33 +0900

    문제 추가

--- src/pages/popup/QuizDialog.js
+++ src/pages/popup/QuizDialog.js
@@ -1,21 +1,45 @@
 import React, { Component } from 'react';
 import Dialog from '@material-ui/core/Dialog'
 import DialogTitle from '@material-ui/core/DialogTitle'
+import DialogContent from '@material-ui/core/DialogContent'
+import DialogContentText from '@material-ui/core/DialogContentText'
+import Fab from '@material-ui/core/Fab'
 
 class QuizDialog extends Component {
   render() {
+    const {quiz} = this.props;
+
     return (
       <Dialog
         aria-labelledby="quiz-dialog-title"
         open={this.props.open}>
         <DialogTitle
           id="quiz-dialog-title">
-          Quiz
+          test
         </DialogTitle>
-
+        <DialogContent>
+          <DialogContentText>
+            {
+              quiz.quizOption ?
+              quiz.quizOption.map(option => (
+                  <Fab>{option}</Fab>
+                )
+              ) :
+              ''
+            }
+          </DialogContentText>
+        </DialogContent>
       </Dialog>
     );
   }
 }
+
+// {
+  // this.props.quiz.quizOption.forEach(option => {
+    // return (<Fab>
+      // {option}
+    // </Fab>)
+  // })
+// }
 
 export default QuizDialog
