8b82435ff1ebc6c5a3d49d83264c42419bfe6dfa
ekim <good_listener@naver.com>
2019-05-28 00:07:17 +0900

    문제 종류 추가

--- src/pages/Room.js
+++ src/pages/Room.js
@@ -13,21 +13,39 @@ class Room extends Component {
     quizList : [
       {
         title : '1번샘플퀴즈',
+        type : 'multiple-choice',
         quizOption : [
           '보기1번',
           '보기2번',
-          '보기3번'
+          '보기3번',
+          '보기4번(정답)'
         ],
-        time : 3000
+        answer : 3,
+        time : 15000
       },
       {
-        title : '2번샘플퀴즈',
+        title : '샘플퀴즈',
+        type : 'multiple-choice',
         quizOption : [
           '보기1번',
           '보기2번',
-          '보기3번'
+          '보기3번(정답)'
         ],
+        answer : 2,
         time : 5000
+      },
+      {
+        title : '[주관식] 나는 ㅁㅁ다.',
+        type : 'open-ended',
+        answer : '바보',
+        time : 15000
+      },
+      {
+        title : '[OX] 사람은 동물이다.',
+        type : 'ox-quiz',
+        quizOption : ['O', 'X'],
+        answer : 'o',
+        time : 3000
       }
     ]
   }
--- src/pages/popup/QuizDialog.js
+++ src/pages/popup/QuizDialog.js
@@ -3,6 +3,7 @@ import Dialog from '@material-ui/core/Dialog'
 import DialogTitle from '@material-ui/core/DialogTitle'
 import DialogContent from '@material-ui/core/DialogContent'
 import DialogContentText from '@material-ui/core/DialogContentText'
+import Button from '@material-ui/core/Button'
 import Fab from '@material-ui/core/Fab'
 
 class QuizDialog extends Component {
@@ -15,22 +16,42 @@ class QuizDialog extends Component {
         open={this.props.open}>
         <DialogTitle
           id="quiz-dialog-title">
-          test
+          {quiz.title}
         </DialogTitle>
         <DialogContent>
           <DialogContentText>
-            {
-              quiz.quizOption ?
-              quiz.quizOption.map(option => (
-                  <Fab>{option}</Fab>
-                )
-              ) :
-              ''
+            { ( () => {
+                if (quiz.type === 'multiple-choice') {
+                  return quiz.quizOption.map(
+                        option => (<Fab onClick={this.handleClickOption}>{option}</Fab>)
+                        )
+                } else if (quiz.type === 'open-ended') {
+                  return (<React.Fragment>
+                          <input onChange={this.handleChangeInput}/>
+                          <Button
+                            variant="contained"
+                            color="primary">
+                            제출
+                          </Button></React.Fragment>
+                        )
+
+                } else if (quiz.type === 'ox-quiz') {
+                  return (<React.Fragment><Fab>O</Fab><Fab>X</Fab></React.Fragment>)
+                }
+              })()
             }
           </DialogContentText>
         </DialogContent>
       </Dialog>
     );
+  }
+
+  handleClickOption = (e) => {
+    console.log(e.target);
+  }
+
+  handleChangeInput = (e) => {
+    console.log(e.target.value);
   }
 }
 
